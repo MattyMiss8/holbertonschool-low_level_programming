87589b5be50730b71bf7de3cad3d3bcd0148992d
MattyMiss8 <9867@holbertonstudents.com>
2024-10-21 08:15:36 +0000

    hello_world

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-gcc -o 4-puts_0 puts"Programming is like building a multilingual puzzle"
+include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
