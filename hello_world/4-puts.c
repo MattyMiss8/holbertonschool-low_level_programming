73c2824bf5d5c040879dc7a3507b51453a789102
MattyMiss8 <9867@holbertonstudents.com>
2024-10-22 05:54:15 +0000

    update betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 /**
  * main - entry point
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
@@ -9,4 +11,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
