428b875f97a1672f8e73ac5e3ba5ce5aa57190a6
MattyMiss8 <9867@holbertonstudents.com>
2024-10-22 06:38:14 +0000

    first attempt

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  printf("lu\n", sizeof (myInt));
+  printf("lu\n", sizeof (myFloat));
+  print("lu\n", sizeof (myDouble));
+  print("lu\n", sizeof (myChar));
+  print ("lu\n", sizeof (myLongInt));
+  print ("lu\n", sizeof (myLongLongInt));
+  return (0);
+}
