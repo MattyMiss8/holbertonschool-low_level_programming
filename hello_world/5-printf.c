eb72873e52d49dafa7c80ebbadf67c46e9d02812
MattyMiss8 <9867@holbertonstudents.com>
2024-10-22 06:10:23 +0000

    first attempt

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,9 +4,10 @@
  * main - entry point
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
 printf("\"with proper grammar, but the outcome is a piece of art,");
-Return (0);
+return (0);
 }
