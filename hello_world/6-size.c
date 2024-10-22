6872ef9faf7b41bfe0b05ab0dc3b02c91acba8cc
MattyMiss8 <9867@holbertonstudents.com>
2024-10-22 06:45:44 +0000

    third attempt

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,9 +10,9 @@ int main(void)
 {
 printf("lu\n", sizeof(myInt));
 printf("lu\n", sizeof(myFloat));
-print("lu\n", sizeof(myDouble));
-print("lu\n", sizeof(myChar));
-print("lu\n", sizeof(myLongInt));
-print("lu\n", sizeof(myLongLongInt));
+printf("lu\n", sizeof(myDouble));
+printf("lu\n", sizeof(myChar));
+printf("lu\n", sizeof(myLongInt));
+printf("lu\n", sizeof(myLongLongInt));
 return (0);
 }
