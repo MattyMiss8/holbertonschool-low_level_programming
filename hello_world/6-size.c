ac6c136312c150de29494ba10a87ab2e5a64ca60
MattyMiss8 <9867@holbertonstudents.com>
2024-10-22 06:43:38 +0000

    third attempt

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,17 +2,17 @@
 
 /**
  * main - entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-  printf("lu\n", sizeof (myInt));
-  printf("lu\n", sizeof (myFloat));
-  print("lu\n", sizeof (myDouble));
-  print("lu\n", sizeof (myChar));
-  print("lu\n", sizeof (myLongInt));
-  print("lu\n", sizeof (myLongLongInt));
+printf("lu\n", sizeof(myInt));
+printf("lu\n", sizeof(myFloat));
+print("lu\n", sizeof(myDouble));
+print("lu\n", sizeof(myChar));
+print("lu\n", sizeof(myLongInt));
+print("lu\n", sizeof(myLongLongInt));
 return (0);
 }
