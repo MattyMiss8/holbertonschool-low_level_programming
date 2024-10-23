84888e16ec076ea698beda368dcb22e7503a478d
MattyMiss8 <9867@holbertonstudents.com>
2024-10-23 08:29:06 +0000

    first attempt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase without q and e
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{
+if (alphabet == 'q' || alphabet == 'e')
+{
+continue;
+}
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
