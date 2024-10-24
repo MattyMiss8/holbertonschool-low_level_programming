031e4f6f7a54582c20cc40822cadee3df7f1e4a0
MattyMiss8 <9867@holbertonstudents.com>
2024-10-24 08:18:14 +0000

    first attempt

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+char c;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (c = 'a'; c < 'g'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
