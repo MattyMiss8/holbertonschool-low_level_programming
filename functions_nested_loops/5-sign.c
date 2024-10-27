9f651368459aae5de6c004c265e3797a408c2832
MattyMiss8 <9867@holbertonstudents.com>
2024-10-27 06:25:41 +0000

    second attempt

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - check the code
+ *
+ * Description: print the sign of a number
+ *
+ * @n: checks for charater
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
+}
