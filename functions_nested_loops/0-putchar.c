c4feb171b4fc90df3d77244c4ae0921ffd02e630
MattyMiss8 <9867@holbertonstudents.com>
2024-10-27 02:17:43 +0000

    first attempt

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main - print _putchar
+ *
+ * Description: print _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+putchar("putchar"\n);
+
+return (0);
+}
