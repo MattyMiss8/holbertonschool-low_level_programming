a51ae5007104081e99e97b8444e33cb8645cd7c1
MattyMiss8 <9867@holbertonstudents.com>
2024-10-27 06:36:18 +0000

    second attempt

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - check the code
+ *
+ * Description: computes the absolute value of an integer
+ *
+ * @r: outputs the absolute value
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int _abs(int r)
+{
+if (r >= 0)
+{
+return (r);
+}
+else
+{
+return (-r);
+}
+
+}
