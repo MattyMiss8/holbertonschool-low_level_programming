71f92384f24885caffc8fdeef0b427ace0c87dcf
MattyMiss8 <9867@holbertonstudents.com>
2024-10-29 07:52:24 +0000

    first attempt

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiple two intergers
+ *
+ * @a: number to multiply
+ *
+ * @b: number to multiply
+ *
+ * Return: Always Success (0)
+ *
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
