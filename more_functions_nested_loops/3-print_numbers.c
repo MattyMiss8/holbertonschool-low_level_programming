dcd6bc217709fe9003d7dbeda26d1831c446a306
MattyMiss8 <9867@holbertonstudents.com>
2024-10-29 08:30:15 +0000

    first attempt

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers from 0 to 9, followed by a new line
+ *
+ *
+ *
+ * Return: Always Success (0)
+ *
+*/
+
+void print_numbers(void)
+{
+int c;
+for (c = '0'; c < '9'; c++)
+{
+_putchar(c);
+}
+_putchar ('\n');
+}
