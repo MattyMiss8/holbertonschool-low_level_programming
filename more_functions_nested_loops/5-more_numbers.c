eebb7e612eeb15e11b7dcc97fd71c24ff0ee301e
MattyMiss8 <9867@holbertonstudents.com>
2024-10-30 06:05:17 +0000

    first attempt

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - print 0 to 14 ten times followed by new line
+ *
+ *
+ *
+ * Return: Always Success (0)
+ *
+*/
+
+void more_numbers(void)
+{
+int c;
+for (c = 48; c < 62; c++)
+{
+_putchar(c);
+}
+_putchar ('\n');
+}
